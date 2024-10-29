fdcad28ed632534669b9de18c99227879d300df2
Ahad Iftikhar <ahadiftikhar77@gmail.com>
2024-10-29 07:48:08 +0500

    Big fix in logout controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -118,8 +118,8 @@ const logoutUser = asyncHandler(async (req, res, next) => {
   await User.findByIdAndUpdate(
     req.user._id,
     {
-      $set: {
-        refreshToken: undefined,
+      $unset: {
+        refreshToken: 1,
       },
     },
     {
